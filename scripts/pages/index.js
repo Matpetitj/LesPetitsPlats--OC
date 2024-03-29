5c386775c44700386729b9f17aa4c5a0b8239fd5
Mathis Petitjean <100531763+Matpetitj@users.noreply.github.com>
2024-03-29 18:06:26 +0100

    review tags area

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,25 +1,44 @@
-// Tableaux des tags
-let allIngredients = [];
-let allAppliances = [];
-let allUstensils = [];
 
+
+// Tableaux de tous les tags séléctionnés depuis les ingredients les appliances et es ustunciles
 let tagsIngredient = [];
 let tagsAppliance = [];
 let tagsUstensils = [];
 
 async function displayData(recipes) {
+// Tableaux de tous les ingredients les appliances et es ustunciles sans doublons
     let allIngredients = [];
     let allAppliances = [];
     let allUstensils = [];
     
-    createTagIngredient();
+   // traitement des données 
     const recipesSection = document.querySelector(".wrapper");
     recipesSection.innerHTML="";
     recipes.forEach((recipe) => {
         const recipeModel = recipeTemplate(recipe);
         const carteRecipe = recipeModel.getRecipesCardDom();
         recipesSection.appendChild(carteRecipe);
+
+        //remplir les tableaux allIng allapliance et allUnst
+        recipe.ingredients.forEach(element => {
+            allIngredients.push(element.ingredient);
+        });
+        
+        recipe.ustensils.forEach(element => {
+            allUstensils.push(element);
+        });
+        allAppliances.push(recipe.appliance);
+               
     });
+    // Eliminer les doublons 
+    allIngredients = new Set(allIngredients);
+    allUstensils = new Set(allUstensils);
+    allAppliances = new Set(allAppliances);
+    console.log(allIngredients);
+    console.log(allUstensils);
+    console.log(allAppliances);
+    // agir sur le dom 
+    createAllTags(allIngredients,allUstensils, allAppliances);
 }
 
 async function init() {
@@ -27,109 +46,89 @@ async function init() {
     displayData(recipes);
 }
 
-function createTagIngredient(ingredients){
+function createAllTags(allIngredients,allUstensils, allAppliances){
+    const tagsSection = document.querySelector(".tagsSection__tagsContainer");
+    // proceder à la création des 3 list all tags 
+    const tagAllIng = createTags(allIngredients, "Ingrédients", "ing");
+    tagsSection.appendChild(tagAllIng);
+
+    const tagAllUst = createTags(allUstensils, "Ustensils", "unst");
+    tagsSection.appendChild(tagAllUst);
+
+    const tagAllAppl = createTags(allAppliances, "Appliances", "appl");
+    tagsSection.appendChild(tagAllAppl);
+}
+
+function createTags(alltags, title){
     
-    const tagsSection = document.querySelector(".tagsSection");
-
-    const tagsContainer = document.createElement('div');
-    tagsContainer.classList.add('tagsSection__tagsContainer');
-
     const tagBtn = document.createElement('div');
     tagBtn.classList.add("tagsSection__tagsContainer__tagBtn");
-
+    
+    const firstBloc = document.createElement('div');
+    firstBloc.classList.add("firstBloc");
+    tagBtn.appendChild(firstBloc);
+    
     const tagTitle = document.createElement('h4');
-    tagTitle.classList.add("tagsSection__tagsContainer__tagBtn__tagTitle");
-    tagTitle.textContent = "Ingrédients";
+    tagTitle.classList.add("tagTitle");
+    tagTitle.textContent = title;
+    firstBloc.appendChild(tagTitle);
 
     const icon = document.createElement('div');
     icon.classList.add('icon');
+    icon.id=id;
+    icon.addEventListener("click", function(event){
+        event.preventDefault();
+        // capter l'id de l'element selectionner 
+        console.log()
+        deuxiemeBloc.style.display = "flex";
+        listTags.style.display = "flex";
+    });
+    firstBloc.appendChild(icon);
 
     const arrowIcon = document.createElement('i');
     arrowIcon.setAttribute("class", "arrowIcon fa-sharp fa-solid fa-angle-down");
+    icon.appendChild(arrowIcon);
+    
+    // creation du deuxieme bloc 
+    const deuxiemeBloc = document.createElement('div');
+    deuxiemeBloc.classList.add("searchBar");
+    tagBtn.appendChild(deuxiemeBloc);
+    const searchBar = document.createElement('input');
+    searchBar.classList.add('input');
+    deuxiemeBloc.appendChild(searchBar);
+    
+    const tagLoupe = document.createElement('i');
+    tagLoupe.setAttribute("class", "tagLoupe fa-solid fa-magnifying-glass");
+    deuxiemeBloc.appendChild(tagLoupe);
 
-    tagBtn.addEventListener("click", function(event){
-        event.preventDefault();
-        const dropdownList = document.querySelector(".dropdownList");
-        dropdownList.style.display = "block";
-        dropdownList.classList.add("open");
+    // creation du troixième bloc
+    const listTags = document.createElement('div');
+    listTags.classList.add("listTags");
+    alltags.forEach(element => {
+        const tag = document.createElement('div');
+        tag.textContent=element;
+        tag.classList.add("tag");
+        listTags.appendChild(tag);
     });
+    
+    tagBtn.appendChild(listTags);
 
-    window.onclick = function(event){
+
+   
+
+ window.onclick = function(event){
         const main = document.querySelector(".main");
         const dropdownList = document.querySelector(".dropdownList");
         if(event.target == main){
-            dropdownList.style.display = "none";
-            dropdownList.classList.remove("open");
+            deuxiemeBloc.style.display = "none";
+            listTags.style.display = "none";
         }
     }
 
-    tagsSection.appendChild(tagsContainer);
-    tagsContainer.appendChild(tagBtn);
-    tagBtn.appendChild(tagTitle);
-    tagBtn.appendChild(icon);
-    icon.appendChild(arrowIcon);
 
-    // AJOUTER UNE CLASSE ET LA RETIRER POUR LE SMOOTH
 
-    const dropdownList = document.createElement('div');
-    dropdownList.classList.add("dropdownList");
-
-    // BARRE DE RECHERCHE DES TAGS
-
-    const tagsForm = document.createElement('form');
-    tagsForm.classList.add('tagsForm');
-
-    const searchBar = document.createElement('input');
-    searchBar.classList.add('tagsForm__searchBar');
-
-    const tagsLoupeBtn = document.createElement('button');
-    tagsLoupeBtn.classList.add('tagsForm__tagsLoupeBtn');
-
-    const tagsLoupe = document.createElement('i');
-    tagsLoupe.setAttribute("class", "tagsLoupe fa-solid fa-magnifying-glass");
-
-    tagsContainer.appendChild(dropdownList);
-    dropdownList.appendChild(tagsForm);
-    tagsForm.appendChild(searchBar);
-    tagsForm.appendChild(tagsLoupeBtn);
-    tagsLoupeBtn.appendChild(tagsLoupe);
-
-    // AFFICHAGE TAGS EN UTILISANT ENCORE DROPDOWNLIST
-
-    const tagsListContainer = document.createElement('div');
-    tagsListContainer.classList.add('tagsListContainer');
-    
-    const selectedTag = document.createElement('div');
-    selectedTag.classList.add('selectedTag');
-
-    const allTags = document.createElement('div');
-    allTags.classList.add('allTags');
-
-    dropdownList.appendChild(tagsListContainer);
-    tagsListContainer.appendChild(selectedTag);
-    tagsListContainer.appendChild(allTags);
-
-    // Nombre recette
-    const totalRecipe = document.createElement('div');
-    totalRecipe.classList.add("tagsSection__totalRecipe");
-
-    const nbrTotalRecipe = document.createElement('p');
-    nbrTotalRecipe.classList.add('tagsSection__totalRecipe__nbrTotalRecipe');
-    nbrTotalRecipe.textContent = "1500 recettes";
-    
-    tagsSection.appendChild(totalRecipe);
-    totalRecipe.appendChild(nbrTotalRecipe);
-
+    return tagBtn;
 }
 
-function fillTags(dom, elements){
-    dom.innerHTML = "";
-    elements.forEach(element => {
-        if ((elements == allIngredients && !tagsIng.includes(element.toLowerCase()))) {
-
-        }
-    })
-
-}
 
 init();
